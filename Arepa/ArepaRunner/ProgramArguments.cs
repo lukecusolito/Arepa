64fcdc9d909eba8f2b3d9c6aedfcedb56af453d6
Luke Cusolito <Luke.Cusolito@outlook.com>
2017-11-30 13:58:16 +1100

    Force remove backslash and quotes from input args

--- Arepa/ArepaRunner/ProgramArguments.cs
+++ Arepa/ArepaRunner/ProgramArguments.cs
@@ -40,14 +40,6 @@ namespace ArepaRunner
         {
             ParseArguments(args);
             LoadAppSettings();
-
-            ProjectDir = PathRemoveBackslash(ProjectDir);
-            OutputDir = PathRemoveBackslash(OutputDir);
-            ProjectFile = PathRemoveBackslash(ProjectFile);
-            AssemblyFile = PathRemoveBackslash(AssemblyFile);
-            TestResultsDir = PathRemoveBackslash(TestResultsDir);
-            MSBuildLocation = PathRemoveBackslash(MSBuildLocation);
-            MSTestLocation = PathRemoveBackslash(MSTestLocation);
         }
 
         public string SelectNewFile(FileType type)
@@ -100,12 +92,12 @@ namespace ArepaRunner
                     {
                         case projectDirArgument:
                             if (args.Length > i + 1)
-                                ProjectDir = args[i + 1];
+                                ProjectDir = PathRemoveBackslash(PathRemoveDoubleQuotes(args[i + 1]));
                             break;
                         case outputDirArgument:
                             if (args.Length > i + 1)
                             {
-                                OutputDir = args[i + 1];
+                                OutputDir = PathRemoveBackslash(PathRemoveDoubleQuotes(args[i + 1]));
                             }
                             break;
                     }
@@ -191,74 +183,36 @@ namespace ArepaRunner
 
         private string PathAddBackslash(string path)
         {
-            // They're always one character but EndsWith is shorter than
-            // array style access to last path character. Change this
-            // if performance are a (measured) issue.
             string separator1 = Path.DirectorySeparatorChar.ToString();
             string separator2 = Path.AltDirectorySeparatorChar.ToString();
-
-            // Trailing white spaces are always ignored but folders may have
-            // leading spaces. It's unusual but it may happen. If it's an issue
-            // then just replace TrimEnd() with Trim(). Tnx Paul Groke to point this out.
+            
             path = path.TrimEnd();
 
-            // Argument is always a directory name then if there is one
-            // of allowed separators then I have nothing to do.
             if (path.EndsWith(separator1) || path.EndsWith(separator2))
                 return path;
 
-            // If there is the "alt" separator then I add a trailing one.
-            // Note that URI format (file://drive:\path\filename.ext) is
-            // not supported in most .NET I/O functions then we don't support it
-            // here too. If you have to then simply revert this check:
-            // if (path.Contains(separator1))
-            //     return path + separator1;
-            //
-            // return path + separator2;
             if (path.Contains(separator2))
                 return path + separator2;
 
-            // If there is not an "alt" separator I add a "normal" one.
-            // It means path may be with normal one or it has not any separator
-            // (for example if it's just a directory name). In this case I
-            // default to normal as users expect.
             return path + separator1;
         }
 
         private string PathRemoveBackslash(string path)
         {
-            // They're always one character but EndsWith is shorter than
-            // array style access to last path character. Change this
-            // if performance are a (measured) issue.
             char separator1 = Path.DirectorySeparatorChar;
             char separator2 = Path.AltDirectorySeparatorChar;
 
-            // Trailing white spaces are always ignored but folders may have
-            // leading spaces. It's unusual but it may happen. If it's an issue
-            // then just replace TrimEnd() with Trim(). Tnx Paul Groke to point this out.
-            //path = path.TrimEnd();
-
-            // Argument is always a directory name then if there is one
-            // of allowed separators then I have nothing to do.
-            //if (!path.EndsWith(separator1) || !path.EndsWith(separator2))
-            //return path;
-
-            // If there is the "alt" separator then I add a trailing one.
-            // Note that URI format (file://drive:\path\filename.ext) is
-            // not supported in most .NET I/O functions then we don't support it
-            // here too. If you have to then simply revert this check:
-            // if (path.Contains(separator1))
-            //     return path + separator1;
-            //
-            // return path + separator2;
             if (path.Contains(separator2.ToString()))
                 return path.TrimEnd(separator2);
 
-            // If there is not an "alt" separator I add a "normal" one.
-            // It means path may be with normal one or it has not any separator
-            // (for example if it's just a directory name). In this case I
-            // default to normal as users expect.
             return path.TrimEnd(separator1);
+        }
+
+        private string PathRemoveDoubleQuotes(string path)
+        {
+            char quoteChar = '"';
+            
+            return path.TrimEnd(quoteChar);
         }
         #endregion
     }
